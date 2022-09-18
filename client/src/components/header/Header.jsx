199166251b086551a86e3d2ef2f6ebbf2be7fee0
Gaurav Singh Rathore <gauravsinghrathorerathore@gmail.com>
2022-09-18 09:58:52 +0530

    Client side changes

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import "./header.css";
+import bgvideo from "../video/vid.mp4"
+import image from "../img/images.jpg"
+export default function Header() {
+  return (
+    <div className="header">
+      {/* <div className="headerTitles">
+        <span className="headerTitleSm">NeuroAI</span>
+        <span className="headerTitleLg">Blog</span>
+      </div > */}
+      {/* <img className="headerImg" src={image} alt="" /> */}
+    </div>
+  );
+}
---
+++ client/src/components/topbar/TopBar.jsx
@@ -0,0 +1,75 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../../context/Context";
+import "./topbar.css";
+
+export default function TopBar() {
+  const { user, dispatch } = useContext(Context);
+  const PF = "http://localhost:5000/images/"
+
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT" });
+  };
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <div className="text">
+          <a href="/">
+          Visioners
+          </a>
+
+        </div>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">
+            <Link className="link" to="/">
+              HOME
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/">
+              ABOUT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/">
+              CONTACT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/write">
+              WRITE
+            </Link>
+          </li>
+          <li className="topListItem" onClick={handleLogout}>
+            {user && "LOGOUT"}
+          </li>
+        </ul>
+      </div>
+      <div className="topRight">
+        {user ? (
+          <Link to="/settings">
+            <img className="topImg" src="https://upleap.com/blog/wp-content/uploads/2018/10/how-to-create-the-perfect-instagram-profile-picture.jpg" alt="" />
+          </Link>
+        ) : (
+          <ul className="topList">
+            <li className="topListItem">
+              <Link className="link" to="/login">
+                LOGIN
+              </Link>
+            </li>
+            <li className="topListItem">
+              <Link className="link" to="/register">
+                REGISTER
+              </Link>
+            </li>
+          </ul>
+        )}
+        <i className="topSearchIcon fas fa-search"></i>
+        <input clasName="gaurav" type="text"  placeholder="Search here"/>
+        
+      </div>
+    </div>
+  );
+}
